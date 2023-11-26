ed8d8a982151397852a74f20a02fbcaf12c51b9f
GVGulov <ghulov@list.ru>
2023-11-26 20:29:56 +0300

    added ability to delete properties

--- client/src/components/nodeInfo/NodeInfo.tsx
+++ client/src/components/nodeInfo/NodeInfo.tsx
@@ -3,12 +3,14 @@ import {graphSlice} from "../../store/reducers/GraphSlice";
 import {IProperty} from "../../entity/graphQuery/IProperty";
 import {INode} from "../../entity/graphQuery/INode";
 import PropertyInfoInput from "./PropertyInfoInput";
+import MyButton from "../UI/MyButton";
 
 
 const NodeInfo = () => {
     const nodeInfoId = useAppSelector(state => state.nodeInfoReducer.nodeInfoId)
     const listNodes = useAppSelector(state => state.graphReducer.nodes)
-    const changeNodeLamda = graphSlice.actions.changeNode
+    const changeNode = graphSlice.actions.changeNode
+
     const dispatch = useAppDispatch()
 
     console.log("nodeInfoId", nodeInfoId)
@@ -21,27 +23,46 @@ const NodeInfo = () => {
         <div>
             <div>{nodeInfo?.label}</div>
             {
-                nodeInfo?.property.map((it) => <div key={it.id}>{it.label}: {it.value}</div>)
+                nodeInfo?.property.map((it) => 
+                    <div style={{display: "flex"}}>
+                        <div key={it.id}> {it.label}: {it.value}</div>
+
+                        <MyButton description={"Удалить"} onClick={()=>{
+
+                            if (nodeInfo != null) {
+
+                                const newNode: INode = {
+                                    id: nodeInfo.id,
+                                    label: nodeInfo.label,
+                                    property: nodeInfo.property.filter((property)=> property.id != it.id)
+                                }
+
+                                dispatch(changeNode(newNode))
+                            }
+                            console.log()
+                        }}/>
+                    </div>
+                )
             }
 
             <PropertyInfoInput onClick={(key, value) => {
+                const id = crypto.randomUUID().toString()
 
                 const property: IProperty = {
-                    id: (Math.random() * 1000).toString(),
+                    id: id,
                     label: key,
                     value: value
                 }
 
                 if (nodeInfo != null) {
 
-                    const changeNode: INode = {
+                    const newNode: INode = {
                         id: nodeInfo.id,
                         label: nodeInfo.label,
                         property: [...nodeInfo.property].concat(property)
                     }
 
-                    console.log("changeNode", changeNode)
-                    dispatch(changeNodeLamda(changeNode))
+                    dispatch(changeNode(newNode))
                 }
             }}/>
 
